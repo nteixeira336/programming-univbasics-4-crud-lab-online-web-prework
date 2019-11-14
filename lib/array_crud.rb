14df3d8caa2e6598770f35e927651352a0536925
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-11-14 13:41:37 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,39 @@
 def create_an_empty_array
-  
+[ ]  
 end
 
-def create_an_array
-  
+def hogwarts_houses
+  ["Gryffindor", "Hufflepuff","Ravenclaw", "Slytherin"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(hogwarts_houses, Stark)
+ hogwarts_hosues<< "Stark" 
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(hogwarts_houses, Lannister)
+ hogwarts_houses.unshift ("Lannister") 
 end
 
 def remove_element_from_end_of_array(array)
-  
+  Stark = hogwarts_houses.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+Lannister=hogwarts_houses.shift  
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def retrieve_element_from_index(array, 0)
+hogwarts_houses [0]   
 end
 
 def retrieve_first_element_from_array(array)
-  
+ hogwarts_houses [0] 
 end
 
 def retrieve_last_element_from_array(array)
-  
+ hogwarts_houses [-1] 
 end
 
 def update_element_from_index(array, index_number, element)
-
+hogwarts_houses[2]= "My house"
 end
